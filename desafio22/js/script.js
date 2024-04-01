6366475b9edef8e5f05f3bc7a514b0a8666e6466
BEATRIZ SOUSA BORGES <beatriz.sborges4@senacsp.edu.br>
2024-04-01 17:21:37 -0300

    respostas

---
+++ desafio22/js/script.js
@@ -0,0 +1,72 @@
+
+
+// 3
+// let p = 1;
+
+// while (p < 5){
+//     console.log(`Repetindo ${p}`)
+//     p++
+// }
+
+
+// tarefa 1
+
+// let number = prompt("digite um numero")
+// let number2 = prompt("digite um numero")
+
+// if (number > number2){
+//     console.log(number)
+// }else {
+//     console.log(number2)
+// }
+
+// tarefa 2
+
+// var number = parseInt(prompt("digite um"));
+
+// if (number % 2 === 0){
+//     alert(number + " é um numero par.");
+// }else {
+//     alert(numero + " é um numero impar.");
+// }
+
+// tarefa 3 
+
+// var number = parseInt(prompt("digite um numero:"))
+
+// console.log("numeros pares entre 1 e " + number + ":")
+// for (var i = 1; i <= number; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// tarefa 4 
+
+// n = parseInt(prompt('digite um numero: '));
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(`${i} * ${n} = ${i * n}`);
+// }
+
+// tarefa 4.2
+
+// let n2 = parseInt(prompt('digite um numero: '));
+// let contador;
+
+// while (contador <= 10) {
+//    console.log(`${contador} * ${n2} = ${contador * n2}`);
+//    contador++
+// }
+
+// tarefa 5
+
+// let n1 = parseInt(prompt("digite um numero"))
+
+// if(n1 > 0){
+//     console.log("seu numero é positivo!")
+// }else if (n1 == 0){
+//     console.log("seu numero é igual a zero")
+// }else {
+//     console.log("seu numero é negativo")
+// }
