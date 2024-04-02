d8395548ef6875743e5a0e7a49d104cb1543d392
BEATRIZ SOUSA BORGES <beatriz.sborges4@senacsp.edu.br>
2024-04-02 15:39:18 -0300

    aula dia 2

--- desafio22/js/script.js
+++ desafio22/js/script.js
@@ -1,15 +1,4 @@
-
-
-// 3
-// let p = 1;
-
-// while (p < 5){
-//     console.log(`Repetindo ${p}`)
-//     p++
-// }
-
-
-// tarefa 1
+              // tarefa 1
 
 // let number = prompt("digite um numero")
 // let number2 = prompt("digite um numero")
@@ -20,7 +9,7 @@
 //     console.log(number2)
 // }
 
-// tarefa 2
+                  // tarefa 2
 
 // var number = parseInt(prompt("digite um"));
 
@@ -30,7 +19,7 @@
 //     alert(numero + " é um numero impar.");
 // }
 
-// tarefa 3 
+                   // tarefa 3 
 
 // var number = parseInt(prompt("digite um numero:"))
 
@@ -41,7 +30,7 @@
 //     }
 // }
 
-// tarefa 4 
+                 // tarefa 4 
 
 // n = parseInt(prompt('digite um numero: '));
 
@@ -49,7 +38,7 @@
 //     console.log(`${i} * ${n} = ${i * n}`);
 // }
 
-// tarefa 4.2
+                 // tarefa 4.2
 
 // let n2 = parseInt(prompt('digite um numero: '));
 // let contador;
@@ -59,7 +48,7 @@
 //    contador++
 // }
 
-// tarefa 5
+                 // tarefa 5
 
 // let n1 = parseInt(prompt("digite um numero"))
 
@@ -69,4 +58,142 @@
 //     console.log("seu numero é igual a zero")
 // }else {
 //     console.log("seu numero é negativo")
+// }
+
+                  // tarefa 6
+
+// var A = prompt("digite uma letra: ")
+// if(A === "a" || A === "e" || A === "i" || A ==="o" || A ==="u"){
+// console.log(`${A} é uma vogal`)
+// }else{
+//     console.log(`${A} é uma consoante`)
+// }
+
+                 // tarefa 7
+
+// let n1 = prompt("digite um némuro: ")
+// if (n1 <= 10){
+//     console.log(`o seu número ${n1}, é valido pois está entre 0 e 10`)
+// }else{
+//     console.log(`o seu número ${n1}, não é valido pois está entre 0 e 10`)
+// }
+
+                    // tarefa 8
+
+// let g = 1
+
+// do{
+//     console.log(`valor de g:${g}`)
+//     g++
+// }while(g <= 100)
+
+                     // tarefa 8.2
+
+// let g = 100
+
+// do{
+//     console.log(`valor de g:${g}`)
+//     g--
+// }while(g >= 1)
+
+                // tarefa 8.3
+
+// let g = 100
+
+// do{
+//     console.log(`valor de g:${g}`)
+//     g -= 5
+// }while(g >= 1)
+
+                  // tarefa 8.4
+
+// let g = 1
+
+// do{
+//     console.log(`valor de g:${g}`)
+//     g +=2
+// }while(g <= 100)
+
+                  // tarefa 8.5
+
+// console.log("numeros de 0 a 100:")
+
+// for(let i = 0; i <= 100; i =+ 5){
+   
+//     console.log(1);
+// }
+                  // tarefa 8.6
+
+// console.log("numeros de 0 a 100:")
+
+// for(let i = 100; i >= 0; --i){
+   
+//     console.log(1);
+// }
+                // tarefa 8.7
+
+// var i = 1;
+
+// while(i <= 100){
+//     console.log(i);
+//     i++;
+// }
+                // tarefa 8.8
+
+// var i = 0;
+
+// while(i <= 100){
+//     console.log(i);
+//     i += 5;
+// }
+                // tarefa 8.9
+
+// var i = 100;
+
+// while(i >= 0){
+//     console.log(i);
+//     i -= 3;
+// }
+
+                // tarefa 9 
+          
+// n1 = parseInt(prompt('digite o primeiro número'));
+// n2 = parseInt(prompt('digite o segundo número'));
+//  let operacao = prompt('digite a operação matemática: ');
+ 
+//  switch(operacao){
+//     case "+":
+//         console.log(`o resultado será: ${n1 + n2}`)
+//         break;
+
+//     case "-":
+//         console.log(`o resultado será: ${n1 - n2}`)
+//         break;
+
+//     case "*":
+//         console.log(`o resultado será: ${n1 * n2}`)
+//         break;
+
+//     case "/":
+//         console.log(`o resultado será: ${n1 / n2}`)
+//         break;
+
+//         default:
+//             console.log('apenas: "+" | "-" | "*" | "/" ');
+//  }
+
+                // tarefa 10 
+
+// let n1 + parseInt(prompt("digite um numero"))
+
+// var nimpar = 0
+
+// while (npar < n1){
+//     npar++;
+//     console.log(`${npar} é impar`)
+//     nimpar++;
+// }if (n1 & 1){
+//     console.log(`o numero escolhido (${n1}) é ímpar`)
+// }else {
+//     console.log(`o numero escolhido (${n1} é par)`)
 // }
